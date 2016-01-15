989243835a73836ef9d4a1f9c2bda1662f63b63d
MrZeRo2000 <MrZeRo2000@mail.ru>
2016-01-15 17:52:40 +0200

    added unit test support and example test unit

---
+++ src/test/java/com/romanpulov/violetnote/Test1.java
@@ -0,0 +1,12 @@
+package com.romanpulov.javafxmaven;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class Test1 {
+
+    @Test
+    public void method1() {
+        assertEquals(1, 2);
+    }
+}
