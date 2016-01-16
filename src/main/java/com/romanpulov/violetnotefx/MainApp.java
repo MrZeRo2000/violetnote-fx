875d11f4b6eacc4f74e9fce4e13fee142d3cd24b
MrZeRo2000 <MrZeRo2000@mail.ru>
2016-01-16 15:57:39 +0200

    tuned plugins to avoid UTF-8 warning

--- src/main/java/com/romanpulov/violetnotefx/MainApp.java
+++ src/main/java/com/romanpulov/violetnotefx/MainApp.java
@@ -7,6 +7,7 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import com.romanpulov.violetnotecore.model.Model1;
 
 public class MainApp extends Application {
 
