d87c9ab59bed7f271de53c351407dec14a325997
MrZeRo2000 <MrZeRo2000@mail.ru>
2016-04-06 13:50:01 +0300

    temp file management and tests

--- src/main/java/com/romanpulov/violetnotefx/FileHelper.java
+++ src/main/java/com/romanpulov/violetnotefx/FileHelper.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.file.FileSystems;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.StandardCopyOption;
 
 /**
@@ -11,7 +12,8 @@ import java.nio.file.StandardCopyOption;
  */
 public class FileHelper {
     private static int FILE_KEEP_COPIES_COUNT = 5;
-    private static String FILE_COPY_FORMAT = "%s.bak%02d";
+    private static final String FILE_COPY_FORMAT = "%s.bak%02d";
+    public static final String FILE_TEMP_EXTENSION = ".temp";
 
     public static boolean saveCopies(String fileName) {
         try {
@@ -22,6 +24,32 @@ public class FileHelper {
                     Files.copy(fc.toPath(), FileSystems.getDefault().getPath(copyFileName), StandardCopyOption.REPLACE_EXISTING);
                 }
             }
+            return true;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    public static String getTempFileName(String fileName) {
+        return fileName + FILE_TEMP_EXTENSION;
+    }
+
+    public static String removeExtension(String fileName, String extension) {
+        return fileName.substring(0, fileName.lastIndexOf(extension));
+    }
+
+    public static String getRevertFileName(String fileName) {
+        return removeExtension(fileName, FILE_TEMP_EXTENSION);
+    }
+
+    public static boolean renameTempFile(String tempFileName) {
+        String targetFileName = getRevertFileName(tempFileName);
+        Path tempFilePath = FileSystems.getDefault().getPath(tempFileName);
+        Path targetFilePath = FileSystems.getDefault().getPath(targetFileName);
+        try {
+            Files.copy(tempFilePath, targetFilePath, StandardCopyOption.REPLACE_EXISTING);
+            Files.delete(tempFilePath);
             return true;
         } catch (IOException e) {
             e.printStackTrace();
--- src/test/java/com/romanpulov/violetnotefx/FileHelperTest.java
+++ src/test/java/com/romanpulov/violetnotefx/FileHelperTest.java
@@ -6,6 +6,7 @@ import java.io.File;
 import java.io.FileOutputStream;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 /**
@@ -14,6 +15,7 @@ import static org.junit.Assert.assertTrue;
 public class FileHelperTest {
 
     private static final String TEST_FILE_NAME = "data\\test.txt";
+    private static final String TEST_NEW_FILE_NAME = "data\\test_new.txt";
 
     @Test
     public void testMethod() {
@@ -30,6 +32,51 @@ public class FileHelperTest {
         s.write("2".getBytes());
         s.flush();
         s.close();
+    }
+
+    @Test
+    public void fileNamesTest() {
+        String fileName = TEST_FILE_NAME;
+        System.out.println("FileName: " + fileName);
+        String tempFileName = FileHelper.getTempFileName(fileName);
+        System.out.println("Temp FileName: " + tempFileName);
+        String revertFileName = FileHelper.removeExtension(tempFileName, FileHelper.FILE_TEMP_EXTENSION);
+        System.out.println("Revert FileName: " + revertFileName);
+        assertEquals(fileName, revertFileName);
+    }
+
+    @Test
+    public void renameFileNameTest() throws Exception{
+        // temp file name
+        String tempFileName = FileHelper.getTempFileName(TEST_NEW_FILE_NAME);
+
+        // delete if temp file exists
+        File f = new File(tempFileName);
+        if (f.exists())
+            f.delete();
+
+
+
+        // create test temp file
+        FileOutputStream s = new FileOutputStream(tempFileName);
+        s.write("XXX".getBytes());
+        s.flush();
+        s.close();
+
+        //check if temp file exists
+        f = new File(tempFileName);
+        assertTrue(f.exists());
+
+        // rename temp file
+        FileHelper.renameTempFile(tempFileName);
+
+        //check temp file does not exist
+        f = new File(tempFileName);
+        assertFalse(f.exists());
+
+        //check if working file exists
+        f = new File(TEST_NEW_FILE_NAME);
+        assertTrue(f.exists());
     }
 
 }
