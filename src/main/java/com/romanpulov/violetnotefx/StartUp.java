08343ebc09050559391f0fbc547b7e4ed7ed85a5
MrZeRo2000 <MrZeRo2000@mail.ru>
2019-09-16 14:32:13 +0300

    changes to support Java 12 and JavaFX as separate package

---
+++ src/main/java/com/romanpulov/violetnotefx/StartUp.java
@@ -0,0 +1,11 @@
+package com.romanpulov.violetnotefx;
+
+/**
+ * A workaround class
+ * https://stackoverflow.com/questions/52653836/maven-shade-javafx-runtime-components-are-missing
+ */
+public class StartUp {
+    public static void main(String[] args) {
+        MainApp.main(args);
+    }
+}
